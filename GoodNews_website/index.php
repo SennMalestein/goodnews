9f0e675e87cbc773834f6372f7b2a73859e43cb4
Florian-Bats <81744660+Florian-Bats@users.noreply.github.com>
2025-03-14 11:48:37 +0100

    styles.css in map + style tweaks

--- GoodNews_website/index.php
+++ GoodNews_website/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Positive News That Inspires</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="CSS/styles.css">
 </head>
 <body>
     <section class="header-main">
